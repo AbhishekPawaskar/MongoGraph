916d5859bf6cfeb7cbc3d2a75e1ada9235ebdc05
Abhishek Pawaskar <abhishek99rp@gmail.com>
2023-02-18 23:25:34 +0530

    setup update

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from setuptools import setup
+with open("README.md", "r") as file:
+    long_description = file.read()
+
 setup(name='mongograph',
-        version='0.0.1',
+        version='0.0.3',
         author='Abhishek Pawaskar',
         description='Build Graphs On MongoDB Easily',
         long_description='Conduct CRUD operations on Graph DataBase on Top of MongoDB',
+        long_description_content_type='text/markdown',
         url='https://github.com/AbhishekPawaskar/MongoGraph',
         keywords='mongograph,grepheasy, graph_easy, GRAPH_EASY',
         python_requires=">=3.10",
